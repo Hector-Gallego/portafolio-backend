08afcb2200575f78e8208dcb3c8819dea8df24e2
Hector-Gallego <gallegohector96@gmail.com>
2023-08-07 14:27:08 -0600

    deploy

---
+++ src/main/java/com/portafolio/backend/portafolio_backend/config/ConfigUserTest.java
@@ -0,0 +1,41 @@
+package com.portafolio.backend.portafolio_backend.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.portafolio.backend.portafolio_backend.model.User;
+import com.portafolio.backend.portafolio_backend.repository.UserRepository;
+
+@Component
+public class ConfigUserTest implements ApplicationRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public ConfigUserTest(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        User user = new User();
+
+        if (userRepository.findByUsername("admin").isEmpty()) {
+
+            user.setUsername("admin");
+            user.setPassword(passwordEncoder.encode("admin"));
+
+            userRepository.save(user);
+
+            System.out.println("Username: admin");
+            System.out.println("Password: admin");
+
+        }
+        ;
+
+    }
+
+}
