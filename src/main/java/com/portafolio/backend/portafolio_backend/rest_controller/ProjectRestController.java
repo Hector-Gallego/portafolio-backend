38ba29ff8ff535d0a3745b32e75ea9bdfb11f4ca
Hector-Gallego <gallegohector96@gmail.com>
2023-08-08 13:17:29 -0500

    Se cambia la configuración de corss a niver controlador

--- src/main/java/com/portafolio/backend/portafolio_backend/rest_controller/ProjectRestController.java
+++ src/main/java/com/portafolio/backend/portafolio_backend/rest_controller/ProjectRestController.java
@@ -9,13 +9,14 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
-
+@CrossOrigin(origins ={"http://localhost:4200, https://hector-gallego.github.io/portafolio/"})
 @RestController
 @RequestMapping("api/projects")
 public class ProjectRestController {
